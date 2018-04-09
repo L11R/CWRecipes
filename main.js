cd429feb6b3aafd3f522d60d90fac02eb6685c0f
L11R <savely@krasovsky.me>
2018-04-09 20:30:59 +0300

    Copy to clipboard button

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 new List('goods', {
     valueNames: ['name', 'type']
 });
+
+const clipboard = new ClipboardJS('.btn');
+
+clipboard.on('success', function() {
+    const copied = $('.copied');
+
+    copied.css("visibility", "visible");
+
+    copied.show();
+    copied.fadeOut(1000);
+});
